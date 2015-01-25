d1b0a1c08645a45b12c307a0889347747ac354a6
mnotti <markusnotti@MacBook-Pro.local>
2015-01-25 13:57:06 -0800

    Initial Commit

---
+++ derivingPi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  derivingPi
+//
+//  Created by Markus Notti on 1/25/15.
+//  Copyright (c) 2015 Markus Notti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ derivingPi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  derivingPi
+//
+//  Created by Markus Notti on 1/25/15.
+//  Copyright (c) 2015 Markus Notti. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
